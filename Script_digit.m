b3f71e264cf3076e01e00cfc90738c6d68fadfb8
CorentinHR <33058637+CorentinHR@users.noreply.github.com>
2018-02-10 17:23:22 +0100

    Update Script_digit.m

--- Script_digit.m
+++ Script_digit.m
@@ -3,13 +3,13 @@
 %
 
 % Lecture de la base de patterns
-data = read_input('C:\Users\Corentin\Documents\TP_Kohonen\data\digit10_16x16_learn.txt');
+data = read_input('C:\Users\Corentin\Documents\TP_Kohonen\data\digit4_16x16_learn.txt');
 
-% Taille d'une imagette carrée
+% Taille d'une imagette carrÃ©e
 pw   = int16(sqrt(data.dim_input)) ;
 
 
-% Création d'une carte de Kohonen
+% CrÃ©ation d'une carte de Kohonen
 N                   = 10 
 learning_rate       = [0.5 0.1] ;       % Initial et final
 neighborhood_spread = [10.0e0 1.0e-1] ;  % Initial et final 3 0.1
@@ -20,13 +20,13 @@ map = SOM_Create2D (N , learning_rate , neighborhood_spread , size_pattern) ;
 % Nettoyage de l'affichage
 clf ;
 
-% Lancement de l'apprentissage sur un certain nombre d'itération
+% Lancement de l'apprentissage sur un certain nombre d'itÃ©ration
 iter_max = 2000 ;
 
-% Mise à jour de la carte pour chaque itération
+% Mise Ã  jour de la carte pour chaque itÃ©ration
 for iter = 1:iter_max
     
-    % Mise à jour de la carte
+    % Mise Ã  jour de la carte
     map = SOM_OneStepLearning (map , data , iter , iter_max) ;
     
     % Construction de l'image des poids des neurones  
@@ -37,4 +37,16 @@ for iter = 1:iter_max
     title(sprintf('T = %d', iter))
     drawnow ;
 end
+pause
+% Phase de test
+data2 = read_input('C:\Users\Corentin\Documents\TP_Kohonen\data\digit4_16x16_test.txt');
+img_test = SOM_BuildImageFromDataThroughMap(map,data2);
+img_base = SOM_BuildImageFromData(data2);
+figure
+subplot(1,2,1)
+imshow(img_test)
+title('test')
+subplot(1,2,2)
+imshow(img_base)
+title('originale')
 
